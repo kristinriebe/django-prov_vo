6e09cb69c5efef8c6a8fef688b87af7b92d49025
Kristin Riebe <kriebe@aip.de>
2017-08-28 10:21:54 +0200

    Some more tests, also for new AGENT option

--- prov_vo/tests.py
+++ prov_vo/tests.py
@@ -8,16 +8,19 @@ from django.test import TestCase
 from django.test import Client
 from django.test.utils import setup_test_environment
 
-from .models import Activity, ActivityFlow, HadStep
+from .models import Activity, ActivityFlow, HadStep, Entity
+from .models import Agent, WasAssociatedWith, WasAttributedTo
 # Run all tests:
 #   python manage.py test
 # or:
 #   python manage.py test --settings=provenance.test_settings --nomigrations
 #
 # Run individual tests using:
-#   python manage.py test prov_vo.tests.Activity_TestCase
+#   python manage.py test --settings=provenance.test_settings --nomigrations prov_vo.tests.Activity_TestCase
 # or similar
 
+
+# Model tests
 class Activity_TestCase(TestCase):
 
     def setUp(self):
@@ -28,7 +31,29 @@ class Activity_TestCase(TestCase):
         qset = Activity.objects.get(id="rave:myid")
         self.assertEqual(qset.name, "mylabel")
 
+class ActivityFlow_TestCase(TestCase):
 
+    def setUp(self):
+        af = ActivityFlow.objects.create(id="rave:flow", name="myflow")
+        af.save()
+
+    def test_getActivity(self):
+        qset = ActivityFlow.objects.get(id="rave:flow")
+        self.assertEqual(qset.name, "myflow")
+
+
+class Entity_TestCase(TestCase):
+
+    def setUp(self):
+        e = Entity.objects.create(id="rave:dr4", name="RAVE DR4")
+        e.save()
+
+    def test_getEntity(self):
+        qset = Entity.objects.get(id="rave:dr4")
+        self.assertEqual(qset.name, "RAVE DR4")
+
+
+# View tests
 class ProvDAL_TestCase(TestCase):
 
     def setUp(self):
@@ -39,6 +64,17 @@ class ProvDAL_TestCase(TestCase):
         h = HadStep.objects.create(activityFlow=af, activity=a)
         h.save()
 
+        e = Entity.objects.create(id="rave:dr4", name="RAVE DR4")
+        e.save()
+
+        ag = Agent.objects.create(id="org:rave", name="RAVE project")
+        ag.save()
+        was = WasAssociatedWith.objects.create(activity=a, agent=ag)
+        was.save()
+        wat = WasAttributedTo.objects.create(entity=e, agent=ag)
+        wat.save()
+
+    # ID is a required parameter
     def test_getProvdalNoID(self):
         client = Client()
         response = client.get(reverse('prov_vo:provdal'))
@@ -55,7 +91,6 @@ class ProvDAL_TestCase(TestCase):
 
     def test_getProvdalActivityFlow(self):
         client = Client()
-        #response = client.get(reverse('prov_vo:index'))
         response = client.get(reverse('prov_vo:provdal')+'?ID=rave:flow')
         self.assertEqual(response.status_code, 200)
 
@@ -65,7 +100,6 @@ class ProvDAL_TestCase(TestCase):
 
     def test_getProvdalActivityFlowDepth2(self):
         client = Client()
-        #response = client.get(reverse('prov_vo:index'))
         response = client.get(reverse('prov_vo:provdal')+'?ID=rave:flow'+'&DEPTH=2')
         self.assertEqual(response.status_code, 200)
         found = re.findall(r"^act.*", response.content, flags=re.MULTILINE)
@@ -75,7 +109,6 @@ class ProvDAL_TestCase(TestCase):
         # If STEPS=TRUE, substeps of the activityFlow shall be followed,
         # thus both, activityFlow and activity must be returned
         client = Client()
-        #response = client.get(reverse('prov_vo:index'))
         response = client.get(reverse('prov_vo:provdal')+'?ID=rave:flow'+'&DEPTH=2'+'&STEPS=TRUE')
         self.assertEqual(response.status_code, 200)
         found = re.findall(r"^act.*", response.content, flags=re.MULTILINE)
@@ -83,8 +116,36 @@ class ProvDAL_TestCase(TestCase):
 
     def test_getProvdalActivityDepth2(self):
         client = Client()
-        #response = client.get(reverse('prov_vo:index'))
         response = client.get(reverse('prov_vo:provdal')+'?ID=rave:act'+'&DEPTH=2')
         self.assertEqual(response.status_code, 200)
         found = re.findall(r"^act.*", response.content, flags=re.MULTILINE)
         self.assertEqual(len(found), 2)
+
+    # In this implementation, ID can also take an agent's ID, but agent relations are only
+    # followed beyond the agent if AGENT option is set to TRUE
+    def test_getProvdalAgent(self):
+        client = Client()
+        response = client.get(reverse('prov_vo:provdal')+'?ID=org:rave&DEPTH=1')
+        self.assertEqual(response.status_code, 200)
+        # only the agent itself should be returned
+        found = re.findall(r"^agent.*", response.content, flags=re.MULTILINE)
+        self.assertEqual(len(found), 1)
+
+    def test_getProvdalAgentFollow(self):
+        client = Client()
+        response = client.get(reverse('prov_vo:provdal')+'?ID=org:rave&AGENT=TRUE&DEPTH=1')
+        self.assertEqual(response.status_code, 200)
+        # agent, activity, entity, wat and was. relation should be returned
+        # strip begin/end document and prefix from response content:
+        content = re.findall(r"^(?!prefix).*", response.content, flags=re.MULTILINE)
+        content = [l for l in content if not l.startswith("document") and not l.startswith("endDocument")]
+        agents = [l for l in content if l.startswith("agent")]
+        was = [l for l in content if l.startswith("wasAssociatedWith")]
+        wat = [l for l in content if l.startswith("wasAttributedTo")]
+        entities = [l for l in content if l.startswith("entity")]
+        activities = [l for l in content if l.startswith("activity")]
+        self.assertEqual(len(agents), 1)
+        self.assertEqual(len(was), 1)
+        self.assertEqual(len(wat), 1)
+        self.assertEqual(len(entities), 1)
+        self.assertEqual(len(activities), 1)
